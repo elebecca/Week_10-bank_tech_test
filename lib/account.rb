e5c2f75089bcf7068587983be44317a898e3b666
Student <student@makerss-air.home>
2020-03-23 14:12:16 +0000

    Add test test for deposit

--- lib/account.rb
+++ lib/account.rb
@@ -4,5 +4,4 @@ class Account
     def initialize
         @balance = 0
     end
-
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -6,4 +6,11 @@ describe Account do
     it 'has an initial balance of 0' do
       expect(account.balance).to equal(0)
     end
+end
+
+describe "#deposit" do
+  it "increases balance" do
+   subject.deposit(100)
+   expect(subject.show_balance).to equal(100)
   end
+end
