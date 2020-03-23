3e79927244930721f3116a69981903ea7c41198e
Student <student@makerss-air.home>
2020-03-23 14:18:15 +0000

    Add deposit and show balance method

--- lib/account.rb
+++ lib/account.rb
@@ -1,7 +1,15 @@
 class Account
     attr_reader :balance
 
-    def initialize
-        @balance = 0
+    def initialize(balance = 0)
+        @balance = balance
+    end
+
+    def deposit(amount)
+        @balance += amount
+    end
+
+    def show_balance
+        @balance
     end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -6,11 +6,13 @@ describe Account do
     it 'has an initial balance of 0' do
       expect(account.balance).to equal(0)
     end
-end
 
-describe "#deposit" do
-  it "increases balance" do
-   subject.deposit(100)
-   expect(subject.show_balance).to equal(100)
+
+  describe "#deposit" do
+    it "increases balance" do
+    subject.deposit(100)
+    expect(subject.show_balance).to equal(100)
+    end
   end
+  
 end
