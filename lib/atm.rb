4dd1a4a6a149493d6de181745d21ebda636bd08f
Student <student@makerss-air.home>
2020-03-25 16:49:04 +0000

    Add deposit and withdraw to Atm- with bug

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,7 +1,20 @@
 class ATM
     attr_reader :account
   
-    def initialize(account)
+    def initialize(account, statement)
       @account = account
+      @statement = statement
+    end
+
+    def deposit(amount)
+        @account.deposit(amount)
+    end
+
+    def withdraw(amount)
+        @account.withdraw(amount)
+    end
+
+    def show_statement
+        @statement.print_statement
     end
 end
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -6,9 +6,30 @@ describe ATM do
   let(:transaction) { double :transaction, type: nil, balance: nil }
   let(:history) { double :history, transactions: [] }
   let(:account) { double :account, amount: 1000, transaction: transaction, history: history }
+  let(:statement) { double :statement }
+  let(:atm) { ATM.new(account, statement) }
+
+
   let(:atm) { ATM.new(account) }
 
   it "is created with an account" do
     expect(atm.account).to be account
   end
+
+  it "is can instruct a statement to print" do
+    expect(statement).to receive(:print_statement)
+    atm.show_statement
+  end
+  it 'allows a user to deposit' do
+    amount = 500
+    expect(account).to receive(:deposit).with(amount)
+    atm.deposit(amount)
+  end
+  
+  it 'allows a user to withdraw' do
+    amount = 500
+    expect(account).to receive(:withdraw).with(amount)
+    atm.withdraw(amount)
+  end
+
 end
